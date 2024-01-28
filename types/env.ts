4d238cf245a5a27bc1fbdb3e3ae46bbe597ab24e
Yash Bharat Rathod <76907769+YASHBITU1@users.noreply.github.com>
2024-01-28 17:21:22 +0530

    Add files via upload

---
+++ types/env.ts
@@ -0,0 +1,7 @@
+export interface ProcessEnv {
+  OPENAI_API_KEY: "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";
+  OPENAI_API_HOST?: string;
+  OPENAI_API_TYPE?: 'openai' | 'azure';
+  OPENAI_API_VERSION?: string;
+  OPENAI_ORGANIZATION?: string;
+}
